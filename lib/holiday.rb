d80c60f34e8701a6113ec0821c8b64692c4a8fe3
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-17 04:47:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -128,7 +128,14 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  
+  holiday_hash.map do |season, holidays|
+    holidays.map do |event, supplies|
+      if supplies.include?("BBQ")
+        event
+      end
+    end
+  end.flatten.compact
+end
   
   
   
